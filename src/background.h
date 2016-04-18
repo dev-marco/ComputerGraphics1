4ef27dbd23db21053310495ed96f831905bc55fc
Marco <marcoaremail@gmail.com>
2016-04-18 03:38:20 -0300

    Changed to work with Color class

--- src/background.h
+++ src/background.h
@@ -1,8 +1,29 @@
 #ifndef SRC_BACKGROUND_H_
 #define SRC_BACKGROUND_H_
 
+#include <memory>
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include "color.h"
+
 class Background {
 
+    public:
+        Background () {};
+        virtual void apply () const {};
+        static const Background NONE;
+};
+
+class BackgroundColor : virtual public Background {
+
+    const Color color;
+
+public:
+    BackgroundColor (const Color &_color) : color(_color) {};
+
+    void apply () const {
+        this->color.apply();
+    }
 };
 
 #endif
