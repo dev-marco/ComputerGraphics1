6865da4251310616f340566e0f86f4858d144ec6
Marco <marcoantonio@dcc.ufmg.br>
2016-04-18 19:01:15 -0300

    Added timeout functions and animate header

--- src/window.h
+++ src/window.h
@@ -1,6 +1,8 @@
 #ifndef SRC_WINDOW_H_
 #define SRC_WINDOW_H_
 
+#include <functional>
+#include <map>
 #include <GLFW/glfw3.h>
 #include "object.h"
 
@@ -8,6 +10,8 @@ class Window {
 
     GLFWwindow *window;
     Object root;
+    std::map<unsigned, std::tuple<std::function<bool()>, double, double>> timeouts;
+    unsigned tick_counter = 0, timeout_counter = 0;
 
 public:
 
@@ -18,7 +22,48 @@ public:
     }
 
     void update () {
-        root.update();
+
+        double now = glfwGetTime();
+        auto timeout = this->timeouts.begin(), timeout_end = this->timeouts.end();
+
+        root.update(now, tick_counter);
+
+        while (timeout != timeout_end) {
+
+            auto next = std::next(timeout, 1);
+
+            if (std::get<1>(timeout->second) <= now) {
+                if (!(std::get<0>(timeout->second)())) {
+                    this->timeouts.erase(timeout);
+                } else {
+                    std::get<1>(timeout->second) += std::get<2>(timeout->second);
+                }
+            }
+
+            timeout = next;
+        }
+
+        tick_counter++;
+    }
+
+    unsigned setTimeout (const std::function<bool()> &func, double interval) {
+
+        double now = glfwGetTime();
+
+        unsigned id = timeout_counter;
+        timeout_counter++;
+
+        this->timeouts[id] = std::forward_as_tuple(func, now + interval, interval);
+
+        return id;
+    }
+
+    void clearTimeout (unsigned id) {
+        this->timeouts.erase(id);
+    }
+
+    unsigned animate (const std::function<bool(double)> &func, double total_time, unsigned steps) {
+
     }
 
     void draw () {
