eda7816619a0ba98a615a9f4e1b18f998f833523
Marco <marcoaremail@gmail.com>
2016-04-19 00:59:52 -0300

    Animation and frame syncing

--- src/main.cc
+++ src/main.cc
@@ -16,8 +16,10 @@ int main (int argc, const char **argv) {
 
     Window window = Window(720, 720, "Trabalho Pratico 1", NULL, NULL);
 
-    Brick *rect = new Brick({-0.1, -0.04, 0.0}, 0.2, 0.08, new BackgroundColor(Color::rgba(100, 0, 255, 0.5)));
-    Object *sphe = new Object({ 0.7,   0.7, -0.5}, true, new Sphere2D({0.0, 0.0}, 0.3), new BackgroundColor(Color::rgba(100, 255, 10, 0.8)));
+    BackgroundColor *rectbg = new BackgroundColor(Color::rgba(100, 0, 255, 0.5));
+    BackgroundColor *sphebg = new BackgroundColor(Color(0.0, 0.0, 0.0, 0.8));
+    Brick *rect = new Brick({-0.1, -0.04, 0.0}, 0.2, 0.08, rectbg);
+    Object *sphe = new Object({ 0.7,   0.7, -0.5}, true, new Sphere2D({0.0, 0.0}, 0.3), sphebg);
     Object *poly = new Object({-0.7,  -0.7, 0.0}, true, new Polygon2D({0.0, 0.0}, 0.3, 5, 0.0), new BackgroundColor(Color::rgba(255, 255, 0, 0.4)));
     window.addObject(rect);
     window.addObject(sphe);
@@ -30,16 +32,16 @@ int main (int argc, const char **argv) {
         window.makeCurrentContext();
 
         id = window.setTimeout([ &window, &id ] () mutable {
-            static int count = 0;
-            std::cout << "timeout " << count << std::endl << "time " << glfwGetTime() << std::endl << std::endl;
-            count++;
-            if (count > 0) {
-                window.clearTimeout(id);
-            }
+            std::cout << "1.5 seconds" << std::endl;
             return true;
         }, 1.5);
 
-        Event::Event<Event::MouseMove>::add(window.get(), [](GLFWwindow *window, double x, double y, double posx, double posy) {
+        window.animate([&sphebg] (double progress) -> bool {
+            sphebg->setColor(Color::rgba(progress * 100, progress * 0, progress * 255, 1.0));
+            return true;
+        }, 10);
+
+        window.event<Event::MouseMove>([](GLFWwindow *window, double x, double y, double posx, double posy) {
 
         }, "mousemove.paddler");
 
@@ -49,8 +51,10 @@ int main (int argc, const char **argv) {
         while (!window.shouldClose()) {
             double ratio;
             int width, height;
+            double start_time = glfwGetTime();
 
-            glfwGetFramebufferSize(window.get(), &width, &height);
+            window.getFramebufferSize(width, height);
+
             ratio = width / (double) height;
             glViewport(0, 0, width, height);
 
@@ -80,6 +84,8 @@ int main (int argc, const char **argv) {
 
             window.swapBuffers();
             glfwPollEvents();
+
+            std::cout << window.sync(start_time, 60) << " FPS" << std::endl;
         }
 
     }
