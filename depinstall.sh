8cb97b29f2da4f8e945ebf8d14a07fa6ee0f4d10
Marco <marcoaremail@gmail.com>
2016-05-08 14:20:43 -0300

    SDL2 and SDL2 Mixer installation

--- depinstall.sh
+++ depinstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 (
-    sudo apt-get install libglew-dev cmake libglu1-mesa-dev freeglut3-dev mesa-common-dev git xorg-dev libgl1-mesa-dev -y
+    sudo apt-get update
+    sudo apt-get install libglew-dev cmake libglu1-mesa-dev freeglut3-dev mesa-common-dev git xorg-dev libgl1-mesa-dev libsdl2-dev libsdl2-mixer-dev -y
     TMP_DIR=`mktemp -d`
     cd ${TMP_DIR}
     git clone https://github.com/glfw/glfw && \
