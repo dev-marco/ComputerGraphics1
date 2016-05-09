5ea1678f66362044566f0eec742ceb90915c500f
Marco <marcoaremail@gmail.com>
2016-05-09 01:48:00 -0300

    Game class with stage management

--- src/breakout/game.cc
+++ src/breakout/game.cc
@@ -1,1 +1,18 @@
 #include "game.h"
+
+namespace Breakout {
+
+    Game::Game (Engine::Window &_window, std::vector<std::string> _stages)
+    : window(_window) {
+        for (const auto &stage : _stages) {
+            this->stages.push(new Stage(this->window, stage));
+        }
+    }
+
+    void Game::clear (void) {
+        while (!this->stages.empty()) {
+            delete this->stages.front();
+            this->stages.pop();
+        }
+    }
+};
--- src/breakout/game.h
+++ src/breakout/game.h
@@ -1,13 +1,49 @@
 #ifndef SRC_BREAKOUT_GAME_H_
 #define SRC_BREAKOUT_GAME_H_
 
+#include <queue>
 #include "stage.h"
+#include "../engine/window.h"
 
 namespace Breakout {
 
     class Game {
 
+        Engine::Window &window;
+        std::queue<Stage *> stages;
 
+        inline void nextStage (void) const {
+            if (!this->stages.empty()) {
+                this->stages.front()->start();
+            } else {
+                std::cout << "YOU WIN" << std::endl;
+            }
+        }
+
+    public:
+
+        Game(Engine::Window &_window, std::vector<std::string> _stages);
+
+        inline ~Game (void) { this->clear(); }
+
+        void clear(void);
+
+        inline void start (void) const { this->nextStage(); }
+
+        inline void update (void) {
+            if (!this->stages.empty()) {
+                Stage *stage = this->stages.front();
+                stage->update();
+                if (stage->won()) {
+                    this->stages.pop();
+                    this->nextStage();
+                    delete stage;
+                } else if (stage->lost()) {
+                    std::cout << "YOU LOSE" << std::endl;
+                    this->clear();
+                }
+            }
+        }
 
     };
 
