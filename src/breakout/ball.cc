1ac2f2c0ba01182be4af0704be21d762fc34af5d
Marco <marcoaremail@gmail.com>
2016-05-09 01:43:31 -0300

    Moved some code to .cc, added sounds and now accepting speed = 0

--- src/breakout/ball.cc
+++ src/breakout/ball.cc
@@ -1,1 +1,43 @@
 #include "ball.h"
+
+namespace Breakout {
+
+    void Ball::onCollision (const Object *other, const std::valarray<double> &point) {
+
+        bool is_brick = other->getType() == "breakout_brick", is_paddler = other->getType() == "breakout_paddler";
+
+        if ((is_brick && static_cast<const Brick *>(other)->brickType() != "abstract_brick") || is_paddler) {
+
+            std::valarray<double>
+                speed = this->getSpeed(),
+                diff = point - this->getPosition(),
+                abs_diff = std::abs(diff);
+
+            if (abs_diff[0] > abs_diff[1]) {
+                if (diff[0] < 0) {
+                    speed[0] = std::abs(speed[0]);
+                } else {
+                    speed[0] = -std::abs(speed[0]);
+                }
+            } else {
+                if (diff[1] < 0) {
+                    speed[1] = std::abs(speed[1]);
+                } else {
+                    speed[1] = -std::abs(speed[1]);
+                }
+            }
+
+            if (is_brick) {
+                this->on_destroy(static_cast<const Brick *>(other));
+                this->sound_brick.play();
+                // deacelerate
+                this->setSpeed(speed * 0.9);
+            } else {
+                this->sound_pop.play();
+                // add paddler speed
+                this->setSpeed(speed + (other->getSpeed() * 0.8));
+            }
+        }
+    }
+
+}
--- src/breakout/ball.h
+++ src/breakout/ball.h
@@ -2,18 +2,28 @@
 #define SRC_BREAKOUT_BALL_H_
 
 #include <valarray>
+#include <memory>
 #include <random>
 #include <chrono>
 #include <cmath>
 #include "brick.h"
 #include "../engine/object.h"
+#include "../engine/audio.h"
 
 namespace Breakout {
 
     class Ball : public Engine::Object {
 
-        Engine::Sphere2D *sphere_mesh, *sphere_collider;
+        std::valarray<double> start_position;
+        std::unique_ptr<Engine::Sphere2D> sphere_mesh, sphere_collider;
+        std::unique_ptr<Engine::BackgroundColor> background_color;
         double max_speed, min_speed;
+        Engine::Audio::Sound
+            sound_pop = Engine::Audio::Sound("audio/ball_pop.ogg"),
+            sound_brick = Engine::Audio::Sound("audio/ball_brick.ogg");
+
+        std::function<void(void)> touch_bottom;
+        std::function<void(const Brick *)> on_destroy;
 
     public:
 
@@ -21,20 +31,31 @@ namespace Breakout {
             return 0.025;
         }
 
-        Ball (double _max_speed, double _min_speed, double _x = 0.0, double _y = 0.0) : Engine::Object(
-            { _x, _y, 4.0 },
+        Ball (
+            double _max_speed,
+            double _min_speed,
+            std::function<void(void)> _touch_bottom,
+            std::function<void(const Brick *)> _on_destroy,
+            const std::valarray<double> &_start_position = { 0.0, 0.0, 0.0 }
+        ) : Engine::Object(
+            _start_position,
             true,
             new Engine::Sphere2D({ 0.0, 0.0, 0.0 }, Ball::DefaultRadius()),
             new Engine::Sphere2D({ 0.0, 0.0, 0.0 }, Ball::DefaultRadius()),
             new Engine::BackgroundColor(Engine::Color::rgba(255, 255, 255, 0.5))
-        ), max_speed(_max_speed), min_speed(_min_speed) {
+        ), start_position(_start_position), max_speed(_max_speed), min_speed(_min_speed), touch_bottom(_touch_bottom), on_destroy(_on_destroy) {
+
+            this->sphere_mesh.reset(static_cast<Engine::Sphere2D *>(this->getMesh()));
+            this->sphere_collider.reset(static_cast<Engine::Sphere2D *>(this->getCollider()));
+            this->background_color.reset(static_cast<Engine::BackgroundColor *>(this->getBackground()));
+        }
+
+        inline void start (void) {
 
             std::mt19937 gen(std::chrono::system_clock::now().time_since_epoch().count());
             std::uniform_real_distribution<double> speed(-1.0, 1.0);
-
-            this->sphere_mesh = static_cast<Engine::Sphere2D *>(this->getMesh());
-            this->sphere_collider = static_cast<Engine::Sphere2D *>(this->getCollider());
             this->setSpeed(Engine::Mesh::resize({ speed(gen), speed(gen), 0.0 }, this->min_speed));
+
         }
 
         inline void afterUpdate (double now, double delta_time, unsigned tick) {
@@ -42,20 +63,25 @@ namespace Breakout {
             std::valarray<double> position = this->getPosition(), speed = this->getSpeed();
             double radius = this->sphere_mesh->getRadius();
 
-            if ((position[0] + radius) >= 1.0) {
-                speed[0] = -std::abs(speed[0]);
-            } else if ((position[0] - radius) <= -1.0) {
-                speed[0] = std::abs(speed[0]);
-            }
+            if ((position[1] - radius) <= -1.0) {
+                this->stop();
+                this->touch_bottom();
+            } else {
+                if ((position[1] + radius) >= 1.0) {
+                    speed[1] = -std::abs(speed[1]);
+                }
 
-            if ((position[1] + radius) >= 1.0) {
-                speed[1] = -std::abs(speed[1]);
-            } else if ((position[1] - radius) <= -1.0) {
-                speed[1] = std::abs(speed[1]);
-                // TODO Game Over
-            }
+                if ((position[0] + radius) >= 1.0) {
+                    speed[0] = -std::abs(speed[0]);
+                } else if ((position[0] - radius) <= -1.0) {
+                    speed[0] = std::abs(speed[0]);
+                }
 
-            this->setSpeed(speed);
+                if ((this->getSpeed() != speed).max()) {
+                    this->sound_pop.play();
+                    this->setSpeed(speed);
+                }
+            }
         }
 
         inline void setRadius (double _radius) {
@@ -67,75 +93,45 @@ namespace Breakout {
             return this->sphere_mesh->getRadius();
         }
 
-        void onCollision (const Object *other, const std::valarray<double> &point) {
+        void onCollision(const Object *other, const std::valarray<double> &point);
 
-            bool is_brick = other->getType() == "breakout_brick", is_paddler = other->getType() == "breakout_paddler";
-
-            if ((is_brick && static_cast<const Brick *>(other)->brickType() != "abstract_brick") || is_paddler) {
-
-                std::valarray<double>
-                    speed = this->getSpeed(),
-                    diff = point - this->getPosition(),
-                    abs_diff = std::abs(diff);
-
-                if (abs_diff[0] > abs_diff[1]) {
-                    if (diff[0] < 0) {
-                        speed[0] = std::abs(speed[0]);
-                    } else {
-                        speed[0] = -std::abs(speed[0]);
-                    }
-                } else {
-                    if (diff[1] < 0) {
-                        speed[1] = std::abs(speed[1]);
-                    } else {
-                        speed[1] = -std::abs(speed[1]);
-                    }
-                }
-
-                if (is_brick) {
-                    // deacelerate
-                    this->setSpeed(speed * 0.9);
-                } else {
-                    // add paddler speed
-                    this->setSpeed(speed + (other->getSpeed() * 0.8));
-                }
-            }
+        void stop (void) {
+            this->setSpeed({ 0.0, 0.0, 0.0 });
+            this->setPosition(this->start_position);
         }
 
         void setSpeed (const std::valarray<double> &_speed, double update = 0.0) {
             std::valarray<double> speed(_speed);
-            double size;
-            constexpr double min_dir_speed = 0.001;
+            double size = Engine::Mesh::norm2(speed);
 
-            if (speed[0] == 0.0) {
-                speed[0] = min_dir_speed;
-            }
+            if (size != 0.0) {
 
-            if (speed[1] == 0.0) {
-                speed[1] = min_dir_speed;
-            }
+                constexpr double min_dir_speed = 0.001;
 
-            if (std::abs(speed[1] / speed[0]) < 0.5) {
-                if (speed[1] < 0) {
-                    speed[1] = std::abs(speed[0]) * -0.5;
-                } else {
-                    speed[1] = std::abs(speed[0]) * 0.5;
+                if (speed[0] == 0.0) {
+                    speed[0] = min_dir_speed;
                 }
-            }
 
-            size = Engine::Mesh::norm(speed);
-
-            if (size > this->max_speed) {
-                speed = Engine::Mesh::resize(speed, size, this->max_speed);
-            } else if (size < this->min_speed) {
-                if (size == 0.0) {
-                    speed = { 1.0, 1.0, 0.0 };
-                    size = std::sqrt(2.0);
+                if (std::abs(speed[1] / speed[0]) < 0.5) {
+                    if (speed[1] < 0) {
+                        speed[1] = std::abs(speed[0]) * -0.5;
+                    } else {
+                        speed[1] = std::abs(speed[0]) * 0.5;
+                    }
                 }
-                speed = Engine::Mesh::resize(speed, size, this->min_speed);
-            }
 
-            this->setPosition(this->getPosition() + speed * update);
+                size = Engine::Mesh::norm(speed);
+
+                if (size > this->max_speed) {
+                    speed = Engine::Mesh::resize(speed, size, this->max_speed);
+                } else if (size < this->min_speed) {
+                    if (size != 0.0) {
+                        speed = Engine::Mesh::resize(speed, size, this->min_speed);
+                    }
+                }
+
+                this->setPosition(this->getPosition() + speed * update);
+            }
 
             Object::setSpeed(speed);
         }
